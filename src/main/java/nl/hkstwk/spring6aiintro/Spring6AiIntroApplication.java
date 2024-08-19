7fff7963a2f4cf982a0810f63c5c24c623c68f27
Harm Kolvoort <hkolvoort+github@gmail.com>
2024-08-19 21:18:20 +0200

    372 - Create New Spring Boot AI Project

---
+++ src/main/java/nl/hkstwk/spring6aiintro/Spring6AiIntroApplication.java
@@ -0,0 +1,13 @@
+package nl.hkstwk.spring6aiintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6AiIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6AiIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/hkstwk/spring6aiintro/Spring6AiIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hkstwk.spring6aiintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AiIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
