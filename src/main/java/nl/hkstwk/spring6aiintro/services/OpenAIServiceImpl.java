b30a588364833db0fccdfefb1efbbaeccdf6e758
Harm Kolvoort <hkolvoort+github@gmail.com>
2024-08-25 11:25:21 +0200

    377 - Test Controller with Postman

--- src/main/java/nl/hkstwk/spring6aiintro/services/OpenAIServiceImpl.java
+++ src/main/java/nl/hkstwk/spring6aiintro/services/OpenAIServiceImpl.java
@@ -1,5 +1,6 @@
 package nl.hkstwk.spring6aiintro.services;
 
+import lombok.extern.slf4j.Slf4j;
 import nl.hkstwk.spring6aiintro.model.Answer;
 import nl.hkstwk.spring6aiintro.model.Question;
 import org.springframework.ai.chat.client.ChatClient;
@@ -8,6 +9,7 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
@@ -29,6 +31,7 @@ public class OpenAIServiceImpl implements OpenAIService {
     @SuppressWarnings("removal")
     @Override
     public Answer getAnswer(Question question) {
+        log.info("Received Question for OpenAI {}", question.question());
         PromptTemplate promptTemplate = new PromptTemplate(question.question());
         Prompt prompt = promptTemplate.create();
         ChatResponse response = chatClient.call(prompt);
